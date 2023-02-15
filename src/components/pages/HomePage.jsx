b1cf0db0852724afc90fd5a94b3ac9804a7195aa
Artem Kraus <pr0gger.dev@gmail.com>
2023-02-15 15:47:08 +0000

    Update HomePage.jsx and home_page.module.css

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,18 +1,19 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import UserDataCard from "../ui/cards/UserDataCard";
 
 import styles from './styles/home_page.module.css';
 
-import testAvatar from "../../../assets/test/testAvatar.jpg";
+import testAvatar from "../../assets/test/testAvatar.jpg";
 
 const HomePage = () => {
-    // const {toastElement} = useContext(ToastContext)
 
     return (
         <main className={styles.main__container}>
             <header className={styles.header__app}>
-                <div className={styles.menu__button}>
-
+                <div className={styles.hamburger_menu__btn}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
                 <div className={styles.settings__buttons}>
 
@@ -30,9 +31,9 @@ const HomePage = () => {
             </div>
 
             <div className={styles.content}>
-                
+
             </div>
-            
+
             <div className={styles.sidebar__right}>
 
             </div>
