965e2c55de055abd6b67ce4d155a19415793951e
Vadim <timecatcher08@gmail.com>
2023-02-19 16:54:20 +0300

    gfroge:fix: left sidebar контент больше не прыгает

--- src/components/ui/cards/UserDataCard.jsx
+++ src/components/ui/cards/UserDataCard.jsx
@@ -1,35 +1,30 @@
-import React, { useContext } from 'react';
-import Avatar from '@mui/material/Avatar';
-import styles from './styles/UserDataCard.module.css';
-import UIStates from '../../../context/UIStates.context';
+import React, { useContext } from "react";
+import Avatar from "@mui/material/Avatar";
+import styles from "./styles/UserDataCard.module.css";
+import UIStates from "../../../context/UIStates.context";
 
 const UserDataCard = ({ name, email, photo }) => {
-  const {sidebars} = useContext(UIStates);
+  const { sidebars } = useContext(UIStates);
 
   return (
-    <section className={styles.user__card}>
-        <Avatar sx={{
-            width: 30,
-            height: 30
-          }} alt="avatar" src={photo}
-        />
+    <section className={styles.user__card + `${!sidebars.isLeftSidebarOpened ? ' ' + styles.closed : ''}`}>
+      <Avatar
+        sx={{
+          width: 40,
+          height: 40,
+        }}
+        alt="avatar"
+        src={photo}
+      />
+      <div className={sidebars.isLeftSidebarOpened ? '' : styles.user__wrap}>
+        <div className={styles.user__info}>
+          <span className={styles.user__name}>{name}</span>
 
-        {
-          sidebars.isLeftSidebarOpened &&
-          <div className={styles.user__info}>
-          
-          <span className={styles.user__name}>
-            {name}
-          </span>
-
-          <span className={styles.user__email}>
-            {email}
-          </span>
-
+          <span className={styles.user__email}>{email}</span>
         </div>
-        }
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default UserDataCard;
